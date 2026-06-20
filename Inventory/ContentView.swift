2415403254202e9a61a18a5ec66de667108bd9c3
Gabrieloide <97452186+gabrieloide@users.noreply.github.com>
2026-06-20 17:09:06 -0400

    Initial Commit

---
+++ Inventory/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Inventory
+//
+//  Created by Gabriel Fuentes on 20/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Inventory/InventoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  InventoryApp.swift
+//  Inventory
+//
+//  Created by Gabriel Fuentes on 20/6/26.
+//
+
+import SwiftUI
+
+@main
+struct InventoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
